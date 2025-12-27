b6ab8665084aa2b87d5b5477f5227916e3b8dad7
Akita-322 <zaharfisyk12343@gmail.com>
2025-12-28 00:32:49 +0300

    Update SpawnBlock mechanic

--- everhold/Scenes/Player/player.gd
+++ everhold/Scenes/Player/player.gd
@@ -1,11 +1,8 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const ROTATION_SPEED = 10
-
-const BLOCK_SCENE = preload("res://Scenes/Block/Block.tscn")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,14 +13,6 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-	if Input.is_action_just_pressed("ui_create_object"):
-		var obj = BLOCK_SCENE.instantiate()
-		get_global_mouse_position()
-		obj.global_position = get_global_mouse_position()
-		add_child(obj)
-		print(obj)
-		pass
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
