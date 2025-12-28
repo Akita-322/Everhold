9c8cfdea8cd944e1194cbb74df5d5f54791bc9a4
Akita-322 <zaharfisyk12343@gmail.com>
2025-12-29 00:31:30 +0300

    Create SAVEMAP file

--- everhold/Scenes/Map/map.gd
+++ everhold/Scenes/Map/map.gd
@@ -4,9 +4,13 @@ extends Node2D
 var ground_level = 0
 var Brick_Block: Vector2i = Vector2i(0, 0)
 var ChangeBLock: bool = false
+const MAPID = 0
+var data = {
+	"id": MAPID,
+	"map": []
+}
 
-
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_create_object"):
 		var mouse_position = get_global_mouse_position()
 		
@@ -19,5 +23,33 @@ func _process(delta: float) -> void:
 		else:
 			Brick_Block = Vector2i(1, 1)
 		ChangeBLock = !ChangeBLock
-	
-	pass
+		
+	if Input.is_action_just_pressed("ui_JSON_test"):
+		print("ui_JSON_test: Action_pressed")
+		for cell in TILEMAP.get_used_cells():
+			var _atlas = TILEMAP.get_cell_atlas_coords(cell)
+			data["map"].append({
+				"pos": [cell.x, cell.y],
+				"type_block": {
+					"atlas": [_atlas.x, _atlas.y]
+				}
+			})
+		var file = FileAccess.open("res://Data/Map/Map.json", FileAccess.WRITE)
+		file.store_string(JSON.stringify(data, "\t"))
+		file.close()
+	if Input.is_action_just_pressed("ui_JSON_test_2"):
+		var file = FileAccess.open("res://Data/Map/Map.json", FileAccess.READ)
+		var text = file.get_as_text()
+		var data = JSON.parse_string(text)
+
+		TILEMAP.clear()
+		for cell_data in data["map"]:
+			var pos = Vector2i(cell_data["pos"][0], cell_data["pos"][1])
+			var atlas = Vector2i(
+				cell_data["type_block"]["atlas"][0],
+				cell_data["type_block"]["atlas"][1]
+				)
+
+			TILEMAP.set_cell(pos, 0, atlas)
+
+		file.close()
