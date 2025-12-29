53b53209c00d9ce77da2069f8d65ea701abc927e
Akita-322 <zaharfisyk12343@gmail.com>
2025-12-29 05:55:19 +0300

    Create loader map and optimize map

--- everhold/Scenes/Map/map.gd
+++ everhold/Scenes/Map/map.gd
@@ -4,52 +4,26 @@ extends Node2D
 var ground_level = 0
 var Brick_Block: Vector2i = Vector2i(0, 0)
 var ChangeBLock: bool = false
-const MAPID = 0
-var data = {
-	"id": MAPID,
-	"map": []
-}
+const MAP = "Map"
 
-func _process(_delta: float) -> void:
+
+func _ready() -> void:
+	MapManager.load_map(MAP, TILEMAP)
+
+func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_create_object"):
 		var mouse_position = get_global_mouse_position()
-		
 		var tile_spawn = TILEMAP.local_to_map(mouse_position)
 		print(tile_spawn)
 		TILEMAP.set_cell(tile_spawn, ground_level, Brick_Block)
+		
 	if Input.is_action_just_pressed("ui_focus_next"):
 		if ChangeBLock:
 			Brick_Block = Vector2i(0, 0)
 		else:
 			Brick_Block = Vector2i(1, 1)
 		ChangeBLock = !ChangeBLock
-		
 	if Input.is_action_just_pressed("ui_JSON_test"):
-		print("ui_JSON_test: Action_pressed")
-		for cell in TILEMAP.get_used_cells():
-			var _atlas = TILEMAP.get_cell_atlas_coords(cell)
-			data["map"].append({
-				"pos": [cell.x, cell.y],
-				"type_block": {
-					"atlas": [_atlas.x, _atlas.y]
-				}
-			})
-		var file = FileAccess.open("res://Data/Map/Map.json", FileAccess.WRITE)
-		file.store_string(JSON.stringify(data, "\t"))
-		file.close()
+		MapManager.save_map(MAP, TILEMAP)
 	if Input.is_action_just_pressed("ui_JSON_test_2"):
-		var file = FileAccess.open("res://Data/Map/Map.json", FileAccess.READ)
-		var text = file.get_as_text()
-		var data = JSON.parse_string(text)
-
-		TILEMAP.clear()
-		for cell_data in data["map"]:
-			var pos = Vector2i(cell_data["pos"][0], cell_data["pos"][1])
-			var atlas = Vector2i(
-				cell_data["type_block"]["atlas"][0],
-				cell_data["type_block"]["atlas"][1]
-				)
-
-			TILEMAP.set_cell(pos, 0, atlas)
-
-		file.close()
+		MapManager.load_map(MAP, TILEMAP)
---
+++ everhold/Scripts/MapManager.gd
@@ -0,0 +1,55 @@
+extends Node
+
+const MAP_DIR := "res://Data/Map/"
+
+# Сохраняем карту по имени
+func save_map(name: String, tilemap: TileMapLayer) -> bool:
+	DirAccess.make_dir_recursive_absolute(MAP_DIR)
+	var data = { "map": [] }
+
+	for cell in tilemap.get_used_cells():
+		var atlas = tilemap.get_cell_atlas_coords(cell)
+		data["map"].append({
+			"pos": [cell.x, cell.y],
+			"type_block": { "atlas": [atlas.x, atlas.y] }
+		})
+	
+	var path = MAP_DIR + name + ".json"
+	var file = FileAccess.open(path, FileAccess.WRITE)
+	if not file:
+		return false
+	file.store_string(JSON.stringify(data))
+	file.close()
+	print("save_map: complete")
+	return true
+	
+# Загружаем карту по имени
+func load_map(name: String, tilemap: TileMapLayer) -> bool:
+	var path = MAP_DIR + name + ".json"
+	if not FileAccess.file_exists(path):
+		return false
+
+	var file = FileAccess.open(path, FileAccess.READ)
+	var text = file.get_as_text()
+	file.close()
+	
+
+	var data = JSON.parse_string(text)
+	if typeof(data) != TYPE_DICTIONARY or not data.has("map"):
+		return false
+
+	tilemap.clear()
+	for cell_data in data["map"]:
+		var pos = Vector2i(cell_data["pos"][0], cell_data["pos"][1])
+		var atlas = Vector2i(
+			cell_data["type_block"]["atlas"][0],
+			cell_data["type_block"]["atlas"][1]
+		)
+		tilemap.set_cell(pos, 0, atlas)
+	print("load_map: complete")
+	return true
+
+## Создать новую пустую карту
+#func new_map(name: String, tilemap: TileMapLayer):
+	#tilemap.clear()
+	#save_map(name, tilemap)
